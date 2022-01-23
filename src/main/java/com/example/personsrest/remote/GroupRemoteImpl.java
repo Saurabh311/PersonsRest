80dae3dc1e5d14dbc424ac3c5f3a968e9427c079
saurabh311 <saurabh311@gmail.com>
2022-01-23 16:31:16 +0100

    All tests green

--- src/main/java/com/example/personsrest/remote/GroupRemoteImpl.java
+++ src/main/java/com/example/personsrest/remote/GroupRemoteImpl.java
@@ -2,75 +2,77 @@ package com.example.personsrest.remote;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Value;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.List;
-import java.util.Objects;
+import reactor.core.publisher.Mono;
 
 public class GroupRemoteImpl implements GroupRemote{
-    WebClient webClient;
 
-    public GroupRemoteImpl(){
-        webClient = WebClient.builder()
-                .baseUrl("https://jsonplaceholder.typicode.com/")
-                .build();
+    private static final String BASE_URL = "/api/groups/";
+    private static final String BASE_URL2 = "/api/groups";
+
+    WebClient webClient;
+    KeyCloakToken token;
+
+    public GroupRemoteImpl() {
+        this.webClient = WebClient.create("https://groups.edu.sensera.se/");
+        this.token = KeyCloakToken.acquire("https://iam.sensera.se/", "test", "group-api", "user", "djnJnPf7VCQvp3Fc")
+                .block();
     }
 
     @Override
     public String getNameById(String groupId) {
-        return (webClient
-                .get()
-                .uri("posts/" + groupId)
+        return webClient.get().uri(BASE_URL + groupId)
+                .header("Authorization", "Bearer " + token.getAccessToken())
                 .retrieve()
                 .bodyToMono(Group.class)
-                .block()).getName();
+                .block().getName();
     }
-
 
     @Override
     public String createGroup(String name) {
-         return  (webClient
-                 .post()
-                .uri("posts/")
-                .body(BodyInserters.fromValue(new Group("1001", name, "xyx")))
+        return webClient.post()
+                .uri(BASE_URL2)
+                .header("Authorization", "Bearer " + token.getAccessToken())
+                .header(HttpHeaders.CONTENT_TYPE , MediaType.APPLICATION_JSON_VALUE)
+                .body(BodyInserters.fromValue(new CreateGroup(name)))
                 .retrieve()
                 .bodyToMono(Group.class)
                 .single()
-                .block()).getName();
+                .block().getId();
     }
 
     @Override
     public String removeGroup(String name) {
-        return Objects.requireNonNull(webClient
-                .delete()
-                .uri("posts/" + name)
+        return webClient.delete().uri(BASE_URL + name)
+                .header("Authorization", "Bearer " + token.getAccessToken())
                 .retrieve()
-                .bodyToMono(Group.class)
-                .block()).getName();
+                .bodyToMono(String.class)
+                .block();
     }
 
     @Value
-
     static class Group {
         String id;
         String name;
-        String body;
-
 
         @JsonCreator
-        public Group(@JsonProperty("id") String id,
-                               @JsonProperty("name") String name,
-                               @JsonProperty("body") String body) {
+        public Group(@JsonProperty("id") String id, @JsonProperty("name") String name) {
             this.id = id;
             this.name = name;
-            this.body = body;
+        }
+    }
 
+    @Value
+    static class CreateGroup {
+        String name;
+
+        @JsonCreator
+        public CreateGroup(@JsonProperty("name") String name) {
+            this.name = name;
         }
     }
 }
